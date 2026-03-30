66f6bca5512dd12f36d49d0d5dbee9a512c5022b
CAIO MUNIZ IHA <caio.iha@e239.local>
2026-03-30 11:09:13 -0300

    simulação de banco

--- aula-03-arquitetura-mvc-e-request/controllers/routes.py
+++ aula-03-arquitetura-mvc-e-request/controllers/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 def init_app(app):
+    
+    listaGames = [{"titulo": "CS-GO", "ano": 2012, "categoria": "FPS Online"}]
+    
     #criando a rota principal do site
     @app.route('/')
     #def serve para criar uma fumção do no python
@@ -37,3 +40,11 @@ def init_app(app):
         
         return render_template('consoles.html',
                             consoles=consoles) 
+        
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        
+        if request.method == 'POST':
+            listaGames.append({'titulo' : request.form.get ('titulo'), 'ano' : request.form.get ('ano'), 'categoria' : request.form.get ('categoria')})
+            return redirect(url_for('cadgames')) 
+        return render_template('cadgames.html', listaGames = listaGames)
